aedf94c917b50da186a2970d78de5db971cd07b0
Kirito <zheng-zh@aeonpeople.biz>
2021-05-25 10:48:24 +0800

    update

--- api/card.js
+++ api/card.js
@@ -1,11 +1,9 @@
 const { graphqlHTTP } = require("express-graphql");
-const { buildSchema } = require("graphql");
 const { query } = require("../config/db");
-// const { card } = require("./card.graphiql");
-
-const schema = await loadSchema("graphql/*.graphql", {
-  loaders: [new GraphQLFileLoader()],
-});
+const { loadSchemaSync } = require("@graphql-tools/load");
+const { GraphQLFileLoader } = require("@graphql-tools/graphql-file-loader");
+const { addResolversToSchema } = require("@graphql-tools/schema");
+const { join } = require("path");
 
 const POKEMON_TABLE_NAME = "POKEMON_CARD_INFO";
 const TRAINER_TABLE_NAME = "TRAINER_CARD_INFO";
@@ -44,66 +42,13 @@ const SQL_GET_TRAINER = ({ category, page }) => {
   FROM ${TRAINER_TABLE_NAME} ${categoryFilter} group by id order by id limit ${page} ,${pageSize}`;
 };
 
-console.log(card);
-
-var schema = buildSchema(`
-    interface res {
-        res:Int
-        errorInfo:String
-    },
-    type powerTypes {
-      "技能费用"
-      cost:String
-      "技能名"
-      skill:String
-      "伤害"
-      damage:String
-      "技能说明"
-      content:String
-    },
-    type pokemonTypes {
-      id:Int
-      pid:String
-      "分类"
-      category:String
-      "名字"
-      name:String
-      "血量"
-      hp:Int
-      "属性"
-      p_type:String
-      "能力"
-      abilitys:[powerTypes]
-      "技能"
-      power:[powerTypes]
-      "弱点属性"
-      weakness_type:String
-      "抵抗属性"
-      resistance_type:String
-      "弱点计算"
-      weakness_number:String
-      "抵抗计算"
-      resistance_number:String
-      "撤退花费"
-      escape:Int
-      "原画"
-      img:String
-    },
-    type trainerRes implements res {
-      res:Int,
-      errorInfo:String,
-      data:[trainerTypes],
-  },
-    type pokemonRes implements res {
-        res:Int,
-        errorInfo:String,
-        data:[pokemonTypes],
-    },
-    type Query {
-      getPokemonList(category:String,page:Int):pokemonRes
-      getTrainerList(category:String,page:Int):trainerRes
-    }
-`);
+// Add resolvers to the schema
+const schemaWithResolvers = addResolversToSchema({
+  schema: loadSchemaSync(join(__dirname, "./card.graphql"), {
+    loaders: [new GraphQLFileLoader()],
+  }),
+  resolvers: {},
+});
 
 var root = {
   getPokemonList: async (req) => {
@@ -159,9 +104,9 @@ var root = {
 
 module.exports = (app) => {
   app.use(
-    "/api/pokemon",
+    "/api/card",
     graphqlHTTP({
-      schema: schema,
+      schema: schemaWithResolvers,
       rootValue: root,
       graphiql: true,
     })
